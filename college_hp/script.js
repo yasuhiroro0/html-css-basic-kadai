d412b2b75c5005310122c3e8607f6cff9191dce0
yasuhiroro0 <160708980+yasuhiroro0@users.noreply.github.com>
2024-08-07 15:58:54 +0900

    ファイルをアップロード
    リスキリング課題の提出

---
+++ college_hp/script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  autoplay:{
+    delay:10000,
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
